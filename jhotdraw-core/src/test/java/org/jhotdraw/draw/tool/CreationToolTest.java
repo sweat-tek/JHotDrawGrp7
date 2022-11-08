b291fa1f5fe449288290293527b227948d027e11
Magnus Kjær Sørensen <magnus.kjaer310101@gmail.com>
2022-11-08 10:32:10 +0100

    Prepare for ellipse tests

---
+++ jhotdraw-core/src/test/java/org/jhotdraw/draw/tool/CreationToolTest.java
@@ -0,0 +1,12 @@
+package org.jhotdraw.draw.tool;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CreationToolTest {
+
+    @Test
+    public void otherTest() {
+        assertEquals(0, 0);
+    }
+}
