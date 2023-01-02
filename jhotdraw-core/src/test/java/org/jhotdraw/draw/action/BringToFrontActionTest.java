7b388e38f0379452c450683ba80e24c4fa9d40b4
Victor <35064405+VictorABoye@users.noreply.github.com>
2023-01-02 19:39:53 +0100

    Readded unit tests

---
+++ jhotdraw-core/src/test/java/org/jhotdraw/draw/action/BringToFrontActionTest.java
@@ -0,0 +1,43 @@
+package org.jhotdraw.draw.action;
+
+import org.jhotdraw.draw.Drawing;
+import org.jhotdraw.draw.DrawingView;
+import org.jhotdraw.draw.QuadTreeDrawing;
+import org.jhotdraw.draw.figure.Figure;
+import org.junit.Test;
+
+import java.awt.geom.Rectangle2D;
+import java.util.*;
+
+import static org.mockito.Mockito.*;
+
+import static org.junit.Assert.*;
+
+public class BringToFrontActionTest {
+
+    @Test
+    public void bringToFront() {
+        // Arrange
+        DrawingView mockView = mock(DrawingView.class);
+        Drawing drawing = new QuadTreeDrawing();
+
+        ArrayList<Figure> figures = new ArrayList<>();
+        Figure figure1 = mock(Figure.class);
+        Figure figure2 = mock(Figure.class);
+        when(figure1.getDrawingArea()).thenReturn(new Rectangle2D.Double());
+        when(figure2.getDrawingArea()).thenReturn(new Rectangle2D.Double());
+        figures.add(figure1);
+        figures.add(figure2);
+        drawing.addAll(figures);
+        when(mockView.getDrawing()).thenReturn(drawing);
+
+
+
+        // Act
+        BringToFrontAction.bringToFront(mockView, Collections.singletonList(figure2));
+        List<Figure> returnedFigures = drawing.getFiguresFrontToBack();
+
+        // Assert
+        assertEquals(returnedFigures.get(0), figure2);
+    }
+}
